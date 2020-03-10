ef84053f3a62dbc109fb4a38c22b5a108224181e
lukelliotfrost <luke@radio.co>
2020-03-10 18:09:50 +0000

    Added Carts route and edited blade file to show the shopping cart on the header of the page.

--- resources/views/front/menu.blade.php
+++ resources/views/front/menu.blade.php
@@ -59,6 +59,7 @@
       </li>
     </ul>
     <li class="list-inline-item"><a href="{{url('/login')}}">Login</a></li>
+    <li class="list-inline-item"><a href="{{url('/cart')}}"><i class="fa fa-shopping-cart">View Cart</i>({{Cart::count()}}) ({{Cart::total()}})</a></li>
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
