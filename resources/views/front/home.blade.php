6593e9597aabff334bf26fb3831eecaf6d94e61c
lukelliotfrost <luke@radio.co>
2020-03-16 17:43:16 +0000

    Added carousel to home page

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -1,19 +1,59 @@
 @extends('front.master')
 
 @section('content')
-<main role="main">
+<br>
+<br>
+<br>
 
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1 class="jumbotron-heading">Album example</h1>
-      <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-      <p>
-        <a href="#" class="btn btn-primary my-2">Main call to action</a>
-        <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-      </p>
-    </div>
-  </section>
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+     <ol class="carousel-indicators">
+       <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+       <li data-target="#myCarousel" data-slide-to="1"></li>
+       <li data-target="#myCarousel" data-slide-to="2"></li>
+     </ol>
+     <div class="carousel-inner">
+       <div class="carousel-item active">
+         <img class="first-slide" src="{{URL::asset('dist/img/create-section1.jpg')}}" alt="First slide">
+         <div class="container">
+           <div class="carousel-caption text-left">
+             <h1>Example headline.</h1>
+             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+             <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+           </div>
+         </div>
+       </div>
+       <div class="carousel-item">
+         <img class="second-slide" src="{{URL::asset('dist/img/explore-section1.jpg')}}" alt="Second slide">
+         <div class="container">
+           <div class="carousel-caption">
+             <h1>Another example headline.</h1>
+             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+             <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+           </div>
+         </div>
+       </div>
+       <div class="carousel-item">
+         <img class="third-slide" src="{{URL::asset('dist/img/rollsroysemain.jpg')}}" alt="Third slide">
+         <div class="container">
+           <div class="carousel-caption text-right">
+             <h1>One more for good measure.</h1>
+             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+             <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+           </div>
+         </div>
+       </div>
+     </div>
+     <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+       <span class="sr-only">Previous</span>
+     </a>
+     <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+       <span class="carousel-control-next-icon" aria-hidden="true"></span>
+       <span class="sr-only">Next</span>
+     </a>
+   </div>
 
+<section>
   <div class="album text-muted">
     <div class="container">
 
@@ -36,6 +76,6 @@
       </div>
     </div>
   </div>
+</section>
 
-</main>
 @endsection
