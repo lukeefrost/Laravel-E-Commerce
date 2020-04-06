eb748f65c08fd3ae10147b593394dff604a4aeb1
lukelliotfrost <luke@radio.co>
2020-04-06 15:57:31 +0100

    Created wishlist function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Product;
 use App\Category;
 use Illuminate\Http\Request;
+use App\WishList;
 
 class HomeController extends Controller
 {
@@ -31,6 +32,18 @@ class HomeController extends Controller
     public function product_details($id)
     {
         $products = Product::findOrFail($id);
+        return view('front.product_details', compact('products'));
+    }
+
+    public function wishList(Request $request)
+    {
+        $wishList = new WishList;
+        $wishList->user_id = Auth::user()->id;
+        $wishList->pro_id = $request->pro_id;
+        $wishList->save();
+
+        $products = DB::table('products')->where('id', $request->pro_id)->get();
+
         return view('front.product_details', compact('products'));
     }
 
